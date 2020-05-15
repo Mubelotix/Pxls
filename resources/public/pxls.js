Mubelotix/Pxls

commit 29265df03d8f66a9f7bd0b277ba580ea5cbf6e7f

Implement `has` function for storageFactory (#339)

resources/public/pxls.js
@@ -32,6 +32,18 @@ window.App = (function() {
       cookieValue += ((exdays == null) ? '' : '; expires=' + exdate.toUTCString());
       document.cookie = cookieName + '=' + cookieValue;
     };
+    const _get = function(name) {
+      let s;
+      if (this.support()) {
+        s = storageType.getItem(name);
+      } else {
+        s = getCookie(prefix + name);
+      }
+      if (s === undefined) {
+        s = null;
+      }
+      return s;
+    };
     return {
       haveSupport: null,
       support: function() {
@@ -47,20 +59,15 @@ window.App = (function() {
         return this.haveSupport;
       },
       get: function(name) {
-        let s;
-        if (this.support()) {
-          s = storageType.getItem(name);
-        } else {
-          s = getCookie(prefix + name);
-        }
-        if (s === undefined) {
-          s = null;
-        }
+        const s = _get(name);
         try {
           return JSON.parse(s);
         } catch (e) {
           return null;
         }
+      },
+      has: function(name) {
+        return _get(name) !== null;
       },
       set: function(name, value) {
         value = JSON.stringify(value);

commit 5c86cbdec5391f38dca783f30d91ba7b4694210c

Fix crash on load caused by #339

resources/public/pxls.js
@@ -32,9 +32,9 @@ window.App = (function() {
       cookieValue += ((exdays == null) ? '' : '; expires=' + exdate.toUTCString());
       document.cookie = cookieName + '=' + cookieValue;
     };
-    const _get = function(name) {
+    const _get = function(name, haveSupport) {
       let s;
-      if (this.support()) {
+      if (haveSupport) {
         s = storageType.getItem(name);
       } else {
         s = getCookie(prefix + name);
@@ -59,7 +59,7 @@ window.App = (function() {
         return this.haveSupport;
       },
       get: function(name) {
-        const s = _get(name);
+        const s = _get(name, this.support());
         try {
           return JSON.parse(s);
         } catch (e) {
@@ -67,7 +67,7 @@ window.App = (function() {
         }
       },
       has: function(name) {
-        return _get(name) !== null;
+        return _get(name, this.support()) !== null;
       },
       set: function(name, value) {
         value = JSON.stringify(value);